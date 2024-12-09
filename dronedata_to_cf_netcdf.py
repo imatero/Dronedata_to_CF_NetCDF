6ab525a21accf37ec42b6aec714af39355b63c33
imatero <43171273+imatero@users.noreply.github.com>
2024-12-09 09:18:15 +0100

    Update dronedata_to_cf_netcdf.py
    Added optional tif to NetCDF transformation step into the script on rhann's request and added some more verbosity to the script.

--- dronedata_to_cf_netcdf.py
+++ dronedata_to_cf_netcdf.py
@@ -2,14 +2,27 @@
 The script reads in the gdal output and metadata fields from separate files
 Script requires the following input files: gdal -file and metadata fields -file
 
-Author: imatero (ilkka.matero@sios-svalbard.org)
-Date: 27.5.2024
+Authors: imatero (ilkka.matero@sios-svalbard.org) and rhann (richard.hann@ntnu.no)
+Date: 7.12.2024
 '''
 
 import xarray as xr
 from lib import data_input_output_functions as io
 
-def main(gdal_fp, metadata_fp, output_fp):
+def main(tif_fp, gdal_fp, metadata_fp, output_fp):
+
+    # Ask user if they want to skip the GDAL transformation step
+    skip_gdal = input("Do you want to skip the GDAL transformation step? (yes/no): ").strip().lower()
+
+    if skip_gdal not in ['yes', 'y']:
+        # Translate TIF into NetCDF
+        print('Translate TIF into NetCDF. This can take a while.')
+        subprocess.run(['gdal_translate', '-of', 'NetCDF', tif_fp, gdal_fp], check=True)
+        print('Done translating TIF into NetCDF')    
+    else:
+        print('Skipping GDAL transformation step.')
+        
+    # Read in NetCDF files
     gdal_in = io.read_in_gdal(gdal_fp)
     
     # Read in metadata
@@ -20,11 +33,13 @@ def main(gdal_fp, metadata_fp, output_fp):
     
     # Write netCDF file
     data_xarray.to_netcdf(output_fp,'w',format='netCDF4')
+    print('Done writing netCDF file') 
     
 if __name__ == "__main__":
     # Specify paths to the input and output files:
-    gdal_fp = 'data/<input_filepath.nc>'
-    metadata_fp = 'data/<metadata_filepath.csv>'
-    output_fp = 'output/<output_filepath>.nc'
+    tif_fp = 'data/01_Lagoon Pingo_2020_DEM.tif'    # Input .tif raw file
+    gdal_fp = 'data/<input_filepath.nc>' # Output .nc NetCDF file
+    metadata_fp = 'data/<metadata_filepath.csv>' # Input .csv file with metadata
+    output_fp = 'output/<output_filepath>.nc' # Output .nc NetCDF file
     
     main(gdal_fp, metadata_fp, output_fp)
